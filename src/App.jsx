8236e99021a8dbdb6dbafb0e63877d8a462a3680
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-17 15:31:21 +0600

    useEffect basic using fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Counter from './Counter'
 import Team from './Team'
+import Users from './Users'
 
 function App() {
   function handleClick(){
@@ -16,6 +17,7 @@ function App() {
   return (
     <>
       <h2>React core concept</h2>
+      <Users></Users>
       <Team></Team>
       <Counter></Counter>
 
---
+++ src/Users.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+export default function Users(){
+  const [users, setUsers] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(data=>setUsers(data))
+
+  },[])
+  return(
+    <div>
+      <h3>Users: {users.length}</h3>
+    </div>
+  )
+}
+
+// decalre a  state to hold the data 
+// useEffect eith call back and dependancy Array
