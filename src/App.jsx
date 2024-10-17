1a7b92df424b69265f76cfcbf55cb0535ffcb723
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-17 16:16:04 +0600

    display data on the react component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Counter from './Counter'
 import Team from './Team'
 import Users from './Users'
+import Friends from './Friends'
 
 function App() {
   function handleClick(){
@@ -17,6 +18,7 @@ function App() {
   return (
     <>
       <h2>React core concept</h2>
+      <Friends></Friends>
       <Users></Users>
       <Team></Team>
       <Counter></Counter>
